073b4f61d1c8fae2553d6705c32aafd28cff7e13
Muhammad Yaseen <66284136+myaseenml@users.noreply.github.com>
2024-08-30 17:09:26 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.utils import platform
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.core.window import Window
 from jnius import autoclass
 
-class MainApp(App):
-    def build(self):
-        layout = BoxLayout(orientation='vertical')
+# Access Android's activity and WebView classes
+PythonActivity = autoclass('org.kivy.android.PythonActivity')
+WebView = autoclass('android.webkit.WebView')
+WebViewClient = autoclass('android.webkit.WebViewClient')
 
-        if platform == 'android':
-            # Load the Android WebView
-            WebView = autoclass('android.webkit.WebView')
-            WebViewClient = autoclass('android.webkit.WebViewClient')
-            activity = autoclass('org.kivy.android.PythonActivity').mActivity
-            webview = WebView(activity)
-            webview.getSettings().setJavaScriptEnabled(True)
-            webview.setWebViewClient(WebViewClient())
-            webview.loadUrl('https://yaseenuom.pythonanywhere.com/')
-            
-            # Add the WebView to the layout
-            layout.add_widget(webview)
-        
-        return layout
+class MyWebApp(BoxLayout):
+    def __init__(self, **kwargs):
+        super(MyWebApp, self).__init__(**kwargs)
+        self.orientation = 'vertical'
+        self.add_widget(Widget())  # Placeholder widget
+
+        self.webview = WebView(PythonActivity.mActivity)
+        self.webview.getSettings().setJavaScriptEnabled(True)
+        self.webview.setWebViewClient(WebViewClient())
+        self.webview.loadUrl('https://yaseenuom.pythonanywhere.com/')
+
+        # Add a button to return to the app
+        self.add_widget(Button(text='Back', size_hint=(1, 0.1), on_release=self.back_to_app))
+
+        # Adding the WebView to the layout
+        Window.add_widget(self.webview)
+
+    def back_to_app(self, instance):
+        # Remove the WebView when returning to the app
+        Window.remove_widget(self.webview)
+
+class MyApp(App):
+    def build(self):
+        return MyWebApp()
 
 if __name__ == '__main__':
-    MainApp().run()
+    MyApp().run()
